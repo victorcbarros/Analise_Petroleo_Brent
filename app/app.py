a950afbddba9985819f756028f1460efa7db12c5
Victor Barros <victorbarros51@gmail.com>
2024-12-02 20:22:53 -0300

    subindo versão do powerbi

--- app/app.py
+++ app/app.py
@@ -13,9 +13,14 @@ with tabs[0]:
     st.markdown(texto_analise, unsafe_allow_html=True)
 
 with tabs[1]:
-    st.header('Análise')
+    st.header('Relatorio PowerBI')
     st.write('Texto sobre o petroleo')
-
+    power_bi_url = "https://app.powerbi.com/reportEmbed?reportId=59968461-603e-47e6-b112-29d809230383&autoAuth=true&ctid=11dbbfe2-89b8-4549-be10-cec364e59551"
+    #st.components.v1.iframe(power_bi_url, width=800, height=600)
+    st.markdown(
+    f"<div style='display: flex; justify-content: center;'><iframe src='{power_bi_url}' width='2000' height='1000' frameborder='0' allowFullScreen='true'></iframe></div>",
+    unsafe_allow_html=True
+)
 with tabs[2]:
     st.header('Previsão')
     st.write('Texto sobre o petroleo')
